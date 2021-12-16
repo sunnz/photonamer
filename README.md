74768f8999963c27dd4b162b2f6e46c95247e03c
sunnz <1378551+sunnz@users.noreply.github.com>
2021-12-16 23:32:55 +1100

    test: update test for getDateMapFromDataOfImages()

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ dev::
     pnpm install
     pnpm dev # watch for file changes and recompile
     pnpm start # in another tab to run the compiled code
+    pnpm test # run tests
--- src/getDateMapFromDataOfImages.test.ts
+++ src/getDateMapFromDataOfImages.test.ts
@@ -13,12 +13,31 @@ test("for an android formatted date there's one or more corresponding src filepa
       src: "photo.jpg",
       date: new Date("1995-12-17T03:24:00"),
       androidDate: "19951217_032400",
-      dst: "IMG_19951217_032400.jpg",
+      dst: "IMG_19951217_032400_01.jpg",
+    },
+    {
+      src: "DS1100.jpg",
+      date: new Date("1995-12-17T03:24:00"),
+      androidDate: "19951217_032400",
+      dst: "IMG_19951217_032400_02.jpg",
+    },
+    {
+      src: "333444999.jpg",
+      date: new Date("2020-08-08T10:42:18"),
+      androidDate: "20200808_104218",
+      dst: "IMG_20200808_104218.jpg",
     },
   ];
 
   const dateMap = getDateMapFromDataOfImages(data);
   expect(dateMap).not.toBeEmpty();
-  expect(dateMap.get("19951217_032400")).not.toBeEmpty();
-  expect(dateMap.get("19951217_032400")?.has("photo.jpg")).toBeTrue();
+  expect(dateMap.size).toBe(2);
+  expect(dateMap.has("19951217_032400")).toBe(true);
+  expect(dateMap.get("19951217_032400")?.has("photo.jpg")).toBe(true);
+  expect(dateMap.get("19951217_032400")?.has("DS1100.jpg")).toBe(true);
+  expect(dateMap.get("19951217_032400")?.has("333444999.jpg")).toBe(false);
+  expect(dateMap.has("20200808_104218")).toBe(true);
+  expect(dateMap.get("20200808_104218")?.has("333444999.jpg")).toBe(true);
+  expect(dateMap.get("20200808_104218")?.has("photo.jpg")).toBe(false);
+  expect(dateMap.get("20200808_104218")?.has("DS1100.jpg")).toBe(false);
 });
